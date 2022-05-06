c5690b119dcbde4c11ffcd26c4b25697c1b204ef
Filip Gadzo <filip.gadzo@fastlane.net>
2022-05-06 20:49:52 +0200

    izvoli

--- app/src/main/java/edu/rit/memeapp/ui/create/MemeCreateListFragment.kt
+++ app/src/main/java/edu/rit/memeapp/ui/create/MemeCreateListFragment.kt
@@ -1,31 +1,32 @@
 package edu.rit.memeapp.ui.create
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import edu.rit.memeapp.R
+import edu.rit.memeapp.databinding.FragmentCreateListBinding
+import edu.rit.memeapp.ui.MemeListViewModel
+
+class MemeCreateListFragment: Fragment() {
+
+    private val viewModel: MemeCreateViewModel by activityViewModels()
+    private lateinit var binding: FragmentCreateListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCreateListBinding.bind(view)
+
+        binding.rvCreate.layoutManager = LinearLayoutManager(requireContext())
 
 
-class MemeCreateListFragment: Fragment(R.layout.fragment_create_list) {
-//    private lateinit var binding: FragmentMemeFavoritesBinding
-//    private lateinit var repo: FavoritesRepository
-//
-//    private val adapter = MemeAdapter {meme ->
-//        val direction = MemeFavoritesFragmentDirections.actionGlobalMemeDetailFragment(meme)
-//        findNavController().navigate(direction)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        binding = FragmentMemeFavoritesBinding.bind(view)
-//        repo = FavoritesRepository(requireContext())
-//
-//        binding.rvFavorites.adapter = adapter
-//
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            val data = repo.getAllMemes()
-//            withContext(Dispatchers.Main){
-//                adapter.setData(data)
-//            }
+
+//        viewModel.memeData.observe(viewLifecycleOwner) { memes ->
+//            adapter.setData(memes)
 //        }
-//    }
+    }
 }
