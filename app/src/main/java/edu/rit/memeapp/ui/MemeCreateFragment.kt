b05d62d8cb0b877633a03ff4819ed5fe032fae81
Filip Gadzo <filip.gadzo@fastlane.net>
2022-05-06 11:11:16 +0200

    Access to gallery and setting image in create view

--- app/src/main/java/edu/rit/memeapp/ui/MemeCreateFragment.kt
+++ app/src/main/java/edu/rit/memeapp/ui/MemeCreateFragment.kt
@@ -1,25 +1,44 @@
 package edu.rit.memeapp.ui
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.provider.MediaStore
+import android.os.Bundle
 import android.view.View
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.widget.Button
+import android.widget.ImageView
 import edu.rit.memeapp.databinding.FragmentMemeCreateBinding
 import androidx.fragment.app.Fragment
 import edu.rit.memeapp.R
+import edu.rit.memeapp.databinding.FragmentMemeListBinding
 
 class MemeCreateFragment: Fragment(R.layout.fragment_meme_create) {
 
     private lateinit var binding: FragmentMemeCreateBinding
-    /*
-    fun access(view: View){
-        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
-        } else {
-            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(gallery, 2)
+    companion object {
+        val IMAGE_REQUEST_CODE = 100
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentMemeCreateBinding.bind(view)
+
+        binding.btnAddImage.setOnClickListener {
+            pickImageGallery()
         }
-    }*/
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            binding.ivAddMeme.setImageURI(data?.data)
+        }
+    }
 }
